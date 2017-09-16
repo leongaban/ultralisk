606941a0dec19b4937af08dcfa06d02a849c4168
leon-yum <leon.gaban@yum.com>
2017-09-16 17:04:48 -0500

    styling updates and created api.js

--- public/containers/Main.js
+++ public/containers/Main.js
@@ -38,11 +38,11 @@ class Main extends Component {
             </h1>
 
             <section>
-              <h3>Hello! We are a <strong><a href="https://lisk.io/">Lisk</a></strong> forging delegate pool.</h3>
+              <h3>Hello! We are a passionate <strong><a href="https://lisk.io/">Lisk</a></strong> forging delegate pool.</h3>
               <ul>
                 <li>Our rewards are 50% back to voters</li>
                 <li>Small pool (7 members max)</li>
-                <li>Weekly payouts (Minimum 1LSK)</li>
+                <li>Weekly payouts (Minimum 1 LSK)</li>
                 <li>Current reward requirements: Vote for <a href="https://explorer.lisk.io/address/1605683440295884021L">ultralisk</a> & <a href="https://explorer.lisk.io/address/16811843780664295310L">leon</a></li>
               </ul>
             </section>
---
+++ public/services/api.js
@@ -0,0 +1,8 @@
+/* eslint-disable (import/prefer-default-export) */
+import axios from 'axios';
+
+const tickerLisk = 'https://api.coinmarketcap.com/v1/ticker/lisk/';
+
+export const getLisk = () => axios.get(tickerLisk)
+  .catch((err) => { throw err; })
+  .then(res => res);
