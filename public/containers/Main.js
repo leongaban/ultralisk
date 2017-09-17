ad5f9968e8ca211a82e5a8c4a57483fcbdf4ab8c
leon-yum <leon.gaban@yum.com>
2017-09-16 21:56:41 -0500

    about us and how we help sections added

--- public/containers/Main.js
+++ public/containers/Main.js
@@ -51,10 +51,7 @@ class Main extends Component {
             <div className="lisk-price" title={roundUSD(price)}>Lisk Price ${round(price)}</div>
             <nav>
               <ul>
-                <li>
-                  <button onClick={() => this.scrollToAbout()}>About</button>
-                  {/* <a href="#about" title="About Ultralisk">About</a> */}
-                </li>
+                <li><button onClick={() => this.scrollToAbout()}>About</button></li>
                 <li><a href="#members" title="All Ultralisk Members">Members</a></li>
                 <li><a href="#rewards" title="Your lisk rewards">My Rewards</a></li>
               </ul>
@@ -87,12 +84,19 @@ class Main extends Component {
           </main>
 
           <section id="about">
-            <h1>About Us</h1>
+            <h2>About Us</h2>
             <p>We are a group of developers, cryptocurrency enthusiasts and friends. As such, we are huge fans of Lisk and all the potential it brings to help expand and grow adoption of blockchain technology.</p>
 
-            <p>This is the dawn of the Internet of Money, and with that comes all the benefits of Decentralized systems and apps. People gaining control back of their own money, new assets being created as better stores of value and wealth, and antiquated financial systems which are prone to hacks being disrupted.</p>
+            <p>This is the dawn of the Internet of Money, and with that comes all the benefits of Decentralized systems and apps. People gaining more control of their own money and new assets being created as better stores of value and wealth.</p>
 
             <p><strong>Lisk</strong> is platform which will help drive this progress forward. And we are here to support the network and give back to the community.</p>
+
+            <h2 className="how-we-help">How we help</h2>
+            <p>We will help the Lisk platform grow by maintaining secure mainnet servers which will process transactions on the network.</p>
+
+            <p>And if we are voted into the top 101 we will give back 50% of our forged Lisk to all our voters. Since we are a small pool, you can vote for us as well as the 2 top pools <a href="https://pool.liskgdt.net/">GDT</a> and <a href="https://liskelite.com/">Elite</a>.</p>
+
+            <p>Click the <strong><a href="#members">Members</a></strong> menu to find out who to vote for.</p>
           </section>
         </div>
       </div>
