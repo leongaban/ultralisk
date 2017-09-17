43016821fcdf4e2c265c047abd05a8f89bee5c45
leon-yum <leon.gaban@yum.com>
2017-09-17 14:40:53 -0500

    footer created and Changelly trade widget added

--- public/components/Members.js
+++ public/components/Members.js
@@ -54,6 +54,20 @@ class Members extends Component {
               </tr>
             </tbody>
           </table>
+
+          <section className="changelly-section">
+            <h2>Get Lisk via Changelly</h2>
+            <iframe
+              src="https://changelly.com/widget/v1?auth=email&from=BTC&to=LSK&merchant_id=167324d59a32&address=&amount=.015&ref_id=167324d59a32&color=594949"
+              width="600"
+              height="360"
+              title="changelly"
+              className="changelly"
+              scrolling="no"
+            >
+              Can't load widget
+            </iframe>
+          </section>
         </div>
       </section>
     );
--- public/containers/Main.js
+++ public/containers/Main.js
@@ -26,6 +26,25 @@ class MainContainer extends Component {
           <About />
           <Members />
         </div>
+        <div className="footer">
+          <ul>
+            <li>
+              <a href="https://discord.gg/fkhNr2f" title="Our discord channel">
+                <img src="static/imgs/discord.png" alt="Our discord channel" />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/leongaban" title="Follow Leon on Twitter">
+                <img src="static/imgs/twitter.png" alt="@leongaban" />
+              </a>
+            </li>
+            <li>
+              <a href="https://coinhover.io/" title="Coinhover crypto calculator">
+                <img src="static/imgs/coinhover.png" alt="Coinhover crypto calculator" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
