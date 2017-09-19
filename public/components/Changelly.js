2109e2ef47f12a0d5509159571c50ce145f80d92
leon-yum <leon.gaban@yum.com>
2017-09-18 21:27:35 -0500

    media querie stylings updated

--- public/components/Changelly.js
+++ public/components/Changelly.js
@@ -3,14 +3,27 @@ import React from 'react';
 const Changelly = () => (
   <section className="changelly-section">
     <h2>Get Lisk via Changelly</h2>
-    <iframe
-      src="https://changelly.com/widget/v1?auth=email&from=BTC&to=LSK&merchant_id=167324d59a32&address=&amount=.015&ref_id=167324d59a32&color=594949"
-      width="600"
-      height="360"
-      title="changelly"
-      className="changelly"
-      scrolling="no"
-    />
+    <div className="hide-640">
+      <iframe
+        src="https://changelly.com/widget/v1?auth=email&from=BTC&to=LSK&merchant_id=167324d59a32&address=&amount=.015&ref_id=167324d59a32&color=594949"
+        width="600"
+        height="360"
+        title="changelly"
+        className="changelly"
+        scrolling="no"
+      />
+    </div>
+
+    <div className="show-640">
+      <iframe
+        src="https://changelly.com/widget/v1?auth=email&from=BTC&to=LSK&merchant_id=167324d59a32&address=&amount=.015&ref_id=167324d59a32&color=594949"
+        width="540"
+        height="360"
+        title="changelly"
+        className="changelly"
+        scrolling="no"
+      />
+    </div>
   </section>);
 
 export default Changelly;
